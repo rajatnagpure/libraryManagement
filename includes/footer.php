4ff60deb55b3a8184e05cfbf42518e24101895d1
Rajat Nagpure <rajatnagpureiiest@gmail.com>
2019-11-30 20:59:34 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   <p> Library Management System | Designed by : ID 510817041-49</p> 
+                </div>
+
+            </div>
+        </div>
+    </section>
